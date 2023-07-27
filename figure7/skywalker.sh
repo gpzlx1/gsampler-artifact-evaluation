319f3b28396881826e09ce6bf98eba116254c4ed
DanielMao1 <daniel4006695539@gmail.com>
2023-07-27 06:26:14 +0000

    add skywalker data script

---
+++ figure7/skywalker.sh
@@ -0,0 +1,20 @@
+grep -Eo 'avg epoch time:[0-9]+\.[0-9]+' outputs/skywalker.log | awk -F':' 'BEGIN{
+    OFS=","; print "baseline,dataset,execution time,algorithm name"}
+    {sub(/ avg epoch time:/,"",$0); 
+    x=int(NR%4); 
+    dataset="unknown"
+    if(x==1)
+        dataset="livejournal";
+    if(x==2)
+        dataset="products";
+    if(x==3)
+        dataset="papers100m";
+    if(x==0)
+        dataset="friendster";
+    algorithm="sage"; 
+    if(NR>4) 
+    algorithm="rw"; 
+    if(NR>8) 
+    algorithm="node2vec"; 
+    printf "skywalker,%s,%s,%s\n", 
+    dataset, $2, algorithm}' >> outputs/result.csv
