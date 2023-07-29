3c9d0a7ec077aceb29747681a0b27e34e106f30a
gpzlx1 <gpzlx1@mail.ustc.edu.cn>
2023-07-29 18:32:09 +0800

    update figure7/gsampler

--- figure7/gSampler/deepwalk_gsampler.py
+++ figure7/gSampler/deepwalk_gsampler.py
@@ -1,43 +1,45 @@
 import torch
-import dgl
-from gs.utils import load_graph
 import time
 import numpy as np
 import gs
 from gs.utils import SeedGenerator
-import sys 
-sys.path.append("..") 
-from ogb.nodeproppred import DglNodePropPredDataset
+import sys
+
+sys.path.append("..")
 import argparse
-from dgl.dataloading import DataLoader, NeighborSampler
 import tqdm
-import scipy.sparse as sp
 import csv
-from load_graph_utils import load_ogbn_products,load_livejournal,load_100Mpapers,load_friendster
+from load_graph_utils import load_ogbn_products, load_livejournal, load_100Mpapers, load_friendster
 
 
-def matrix_batch_sampler_deepwalk(A: gs.Matrix, seeds, num_steps):
-    path = A._graph._CAPI_random_walk(seeds,num_steps)
-    return path
-
+def randomwalk_sampler(A, seeds, seeds_ptr, walk_length):
+    return A.random_walk(seeds, seeds_ptr, walk_length)
 
 
 def benchmark_w_o_relabel(args, matrix, nid):
-    print('####################################################gSampler deepwalk')
+    print(
+        '####################################################gSampler deepwalk'
+    )
     # sampler = DeepWalkSampler(args.walk_length)
-    print("train id size:",len(nid))
+    print("train id size:", len(nid))
     batch_size = args.big_batch
-    seedloader = SeedGenerator(
-        nid, batch_size=batch_size, shuffle=True, drop_last=False)
-    # train_dataloader = DataLoader(g, train_nid, sampler,batch_size=config['batch_size'], use_prefetch_thread=False,
-    # shuffle=False,drop_last=False, num_workers=config['num_workers'],device='cuda',use_uva=config['use_uva'])
-    
+    seedloader = SeedGenerator(nid,
+                               batch_size=batch_size,
+                               shuffle=True,
+                               drop_last=False)
 
     small_batch_size = args.batchsize
 
-    #orig_seeds_ptr = torch.arange(num_batches + 1, dtype=torch.int64, device='cuda') * small_batch_size
     print(args.num_epoch, batch_size, small_batch_size)
-    
+
+    # compile
+    num_batches = int((batch_size + small_batch_size - 1) / small_batch_size)
+    orig_seeds_ptr = torch.arange(
+        num_batches + 1, dtype=torch.int64, device="cuda") * small_batch_size
+    compile_func = gs.jit.compile(randomwalk_sampler,
+                                  args=(matrix, seedloader.data[:batch_size],
+                                        orig_seeds_ptr, args.walk_length))
+
     epoch_time = []
     mem_list = []
     torch.cuda.synchronize()
@@ -45,34 +47,36 @@ def benchmark_w_o_relabel(args, matrix, nid):
     print('memory allocated before training:',
           static_memory / (1024 * 1024 * 1024), 'GB')
     for epoch in range(args.num_epoch):
-        num_batches = int((batch_size + small_batch_size - 1) / small_batch_size)
+        num_batches = int(
+            (batch_size + small_batch_size - 1) / small_batch_size)
         torch.cuda.reset_peak_memory_stats()
         torch.cuda.synchronize()
         start = time.time()
         for it, seeds in enumerate(tqdm.tqdm(seedloader)):
-            seeds = seeds.to('cuda')
-           # seeds_ptr = orig_seeds_ptr
+            seeds_ptr = orig_seeds_ptr
             if it == len(seedloader) - 1:
-                num_batches = int((seeds.numel() + small_batch_size - 1) / small_batch_size)
-            paths = matrix_batch_sampler_deepwalk(matrix, seeds, args.walk_length)
-            # print("paths:",paths.shape,"num_batches:",num_batches)
-            split_paths = torch.tensor_split(paths,num_batches)
-            # print(len(split_paths))
-            # print(split_paths[0].shape)
-            # print(len(ptrts[0][0]),len(indts[0][0]))
-            # print(len(ptrts[1][0]),len(indts[1][0]))
+                num_batches = int(
+                    (seeds.numel() + small_batch_size - 1) / small_batch_size)
+                seeds_ptr = torch.arange(num_batches + 1,
+                                         dtype=torch.int64,
+                                         device='cuda') * small_batch_size
+                seeds_ptr[-1] = seeds.numel()
+            ret = compile_func(matrix, seeds, seeds_ptr, args.walk_length)
 
         torch.cuda.synchronize()
         epoch_time.append(time.time() - start)
-        mem_list.append((torch.cuda.max_memory_allocated() -
-                        static_memory) / (1024 * 1024 * 1024))
+        mem_list.append((torch.cuda.max_memory_allocated() - static_memory) /
+                        (1024 * 1024 * 1024))
 
-        print("Epoch {:05d} | Epoch Sample Time {:.4f} s | GPU Mem Peak {:.4f} GB"
-              .format(epoch, epoch_time[-1], mem_list[-1]))
+        print(
+            "Epoch {:05d} | Epoch Sample Time {:.4f} s | GPU Mem Peak {:.4f} GB"
+            .format(epoch, epoch_time[-1], mem_list[-1]))
 
     # use the first epoch to warm up
-    print(f'Average epoch sampling time: mean: {np.mean(epoch_time[1:])}, variance: {np.var(epoch_time[1:])}, min: {np.min(epoch_time[1:])}, max: {np.max(epoch_time[1:])}')
-    print('Average epoch gpu mem peak:', np.mean(mem_list[1:])," GB")
+    print(
+        f'Average epoch sampling time: mean: {np.mean(epoch_time[1:])}, variance: {np.var(epoch_time[1:])}, min: {np.min(epoch_time[1:])}, max: {np.max(epoch_time[1:])}'
+    )
+    print('Average epoch gpu mem peak:', np.mean(mem_list[1:]), " GB")
     print('####################################################END')
     with open("../outputs/result.csv", "a") as f:
         writer = csv.writer(f, lineterminator="\n")
@@ -80,14 +84,16 @@ def benchmark_w_o_relabel(args, matrix, nid):
         log_info = ["gSampler", args.dataset, np.mean(epoch_time[1:]), "rw"]
         writer.writerow(log_info)
         print(f"result writen to ../outputs/result.csv")
-def load(dataset,args):
+
+
+def load(dataset, args):
     device = args.device
     use_uva = args.use_uva
     g, features, labels, n_classes, splitted_idx = dataset
     sample_list = []
     static_memory = torch.cuda.memory_allocated()
     train_nid = splitted_idx['train']
-    
+
     if args.data_type == 'int':
         g = g.int()
         train_nid = train_nid.int()
@@ -98,9 +104,7 @@ def load(dataset,args):
         g = g.long()
         train_nid = train_nid.long()
     csc_indptr, csc_indices, edge_ids = g.adj_sparse('csc')
-    # train_nid = train_nid.int()
-    # csc_indptr = csc_indptr.int()
-    # csc_indices = csc_indices.int()
+
     if use_uva and device == 'cpu':
         csc_indptr = csc_indptr.pin_memory()
         csc_indices = csc_indices.pin_memory()
@@ -108,34 +112,53 @@ def load(dataset,args):
         csc_indptr = csc_indptr.to('cuda')
         csc_indices = csc_indices.to('cuda')
 
-    m = gs.Matrix(gs.Graph(False))
-    m._graph._CAPI_load_csc(csc_indptr, csc_indices)
-    print("Check load successfully:", m._graph._CAPI_metadata(), '\n')
+    m = gs.matrix_api.Matrix()
+    m.load_graph("CSC", [csc_indptr, csc_indices])
+    bm = gs.matrix_api.BatchMatrix()
+    bm.load_from_matrix(m, False)
     train_nid = train_nid.to('cuda')
-    # csc_indptr, csc_indices, edge_ids = g.adj_sparse('csc')
-    del g
-    benchmark_w_o_relabel(args, m, train_nid)
 
+    del g
+    benchmark_w_o_relabel(args, bm, train_nid)
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--device", default='cuda', choices=['cuda', 'cpu'],
+    parser.add_argument("--device",
+                        default='cuda',
+                        choices=['cuda', 'cpu'],
                         help="Training model on gpu or cpu")
-    parser.add_argument('--use-uva', action=argparse.BooleanOptionalAction,
-                        help="Wether to use UVA to sample graph and load feature")
-    parser.add_argument("--dataset", default='products', choices=['reddit', 'products', 'papers100m','friendster','livejournal'],
+    parser.add_argument(
+        '--use-uva',
+        action=argparse.BooleanOptionalAction,
+        help="Wether to use UVA to sample graph and load feature")
+    parser.add_argument("--dataset",
+                        default='products',
+                        choices=[
+                            'reddit', 'products', 'papers100m', 'friendster',
+                            'livejournal'
+                        ],
                         help="which dataset to load for training")
-    parser.add_argument("--batchsize", type=int, default=128,
+    parser.add_argument("--batchsize",
+                        type=int,
+                        default=128,
                         help="batch size for training")
-    parser.add_argument("--num-epoch", type=int, default=6,
+    parser.add_argument("--num-epoch",
+                        type=int,
+                        default=6,
                         help="numbers of epoch in training")
-    parser.add_argument("--data-type", default='long', choices=['int', 'long'],
+    parser.add_argument("--data-type",
+                        default='long',
+                        choices=['int', 'long'],
                         help="data type")
-    parser.add_argument("--walk-length", type=int, default=80,
+    parser.add_argument("--walk-length",
+                        type=int,
+                        default=80,
                         help="random walk walk length")
-    parser.add_argument("--big-batch", type=int, default=1280,
+    parser.add_argument("--big-batch",
+                        type=int,
+                        default=1280,
                         help="big batch")
     args = parser.parse_args()
     print('Loading data')
@@ -148,4 +171,4 @@ if __name__ == '__main__':
     elif args.dataset == 'livejournal':
         dataset = load_livejournal()
     print(dataset[0])
-    load(dataset,args)
+    load(dataset, args)
--- figure7/gSampler/node2vec_gsampler.py
+++ figure7/gSampler/node2vec_gsampler.py
@@ -1,55 +1,59 @@
 import torch
 import dgl
-from gs.utils import load_graph
 import time
 import numpy as np
 import gs
 from gs.utils import SeedGenerator
-import sys 
-sys.path.append("..") 
-from ogb.nodeproppred import DglNodePropPredDataset
+import sys
+
+sys.path.append("..")
 import argparse
-from dgl.dataloading import DataLoader, NeighborSampler
 import tqdm
-import scipy.sparse as sp
 import csv
-from load_graph_utils import load_ogbn_products,load_livejournal,load_100Mpapers
+from load_graph_utils import load_ogbn_products, load_livejournal, load_100Mpapers
+
 
 def load_friendster():
     train_id = torch.load("/home/ubuntu/dataset/friendster_trainid_10x.pt")
     splitted_idx = dict()
-    splitted_idx['train']=train_id
+    splitted_idx['train'] = train_id
     bin_path = "/home/ubuntu/dataset/friendster/friendster_adj.bin"
     g_list, _ = dgl.load_graphs(bin_path)
     g = g_list[0]
     print("graph loaded")
     print(g.formats())
     # g = g.formats("csc")
-    g=g.long()
-    return g, None,None,None,splitted_idx
+    g = g.long()
+    return g, None, None, None, splitted_idx
 
-def matrix_batch_sampler_deepwalk(A: gs.Matrix, seeds, num_steps):
-    path = A._graph._CAPI_node2vec_random_walk(seeds,num_steps,2,0.5)
-    return path
 
+def node2vec_sampler(A, seeds, num_batch, walk_length, p, q):
+    return A.node2vec(seeds, num_batch, walk_length, p, q)
 
 
 def benchmark_w_o_relabel(args, matrix, nid):
     print('####################################################DGL deepwalk')
     # sampler = DeepWalkSampler(args.walk_length)
-    print("train id size:",len(nid))
+    print("train id size:", len(nid))
     batch_size = args.big_batch
-    seedloader = SeedGenerator(
-        nid, batch_size=batch_size, shuffle=True, drop_last=False)
-    # train_dataloader = DataLoader(g, train_nid, sampler,batch_size=config['batch_size'], use_prefetch_thread=False,
-    # shuffle=False,drop_last=False, num_workers=config['num_workers'],device='cuda',use_uva=config['use_uva'])
-    
+    seedloader = SeedGenerator(nid,
+                               batch_size=batch_size,
+                               shuffle=True,
+                               drop_last=False)
 
     small_batch_size = args.batchsize
-
-    #orig_seeds_ptr = torch.arange(num_batches + 1, dtype=torch.int64, device='cuda') * small_batch_size
     print(args.num_epoch, batch_size, small_batch_size)
-    
+
+    # compile
+    num_batches = int((batch_size + small_batch_size - 1) / small_batch_size)
+    orig_seeds_ptr = torch.arange(
+        num_batches + 1, dtype=torch.int64, device="cuda") * small_batch_size
+    compile_func = gs.jit.compile(node2vec_sampler,
+                                  args=(matrix, seedloader.data[:batch_size],
+                                        orig_seeds_ptr, args.walk_length, 2,
+                                        0.5))
+    print("Finish compiling")
+
     epoch_time = []
     mem_list = []
     torch.cuda.synchronize()
@@ -57,48 +61,56 @@ def benchmark_w_o_relabel(args, matrix, nid):
     print('memory allocated before training:',
           static_memory / (1024 * 1024 * 1024), 'GB')
     for epoch in range(args.num_epoch):
-        num_batches = int((batch_size + small_batch_size - 1) / small_batch_size)
+        num_batches = int(
+            (batch_size + small_batch_size - 1) / small_batch_size)
         torch.cuda.reset_peak_memory_stats()
         torch.cuda.synchronize()
         start = time.time()
         for it, seeds in enumerate(tqdm.tqdm(seedloader)):
-            seeds = seeds.to('cuda')
+            seeds_ptr = orig_seeds_ptr
             if it == len(seedloader) - 1:
-                break
-                num_batches = int((seeds.numel() + small_batch_size - 1) / small_batch_size)
-            paths = matrix_batch_sampler_deepwalk(matrix, seeds, args.walk_length)
-            split_paths = torch.tensor_split(paths,num_batches)
+                num_batches = int(
+                    (seeds.numel() + small_batch_size - 1) / small_batch_size)
+                seeds_ptr = torch.arange(num_batches + 1,
+                                         dtype=torch.int64,
+                                         device='cuda') * small_batch_size
+                seeds_ptr[-1] = seeds.numel()
+            ret = compile_func(matrix, seeds, seeds_ptr, args.walk_length, 2,
+                               0.5)
 
         torch.cuda.synchronize()
         epoch_time.append(time.time() - start)
-        mem_list.append((torch.cuda.max_memory_allocated() -
-                        static_memory) / (1024 * 1024 * 1024))
+        mem_list.append((torch.cuda.max_memory_allocated() - static_memory) /
+                        (1024 * 1024 * 1024))
 
-        print("Epoch {:05d} | Epoch Sample Time {:.4f} s | GPU Mem Peak {:.4f} GB"
-              .format(epoch, epoch_time[-1], mem_list[-1]))
+        print(
+            "Epoch {:05d} | Epoch Sample Time {:.4f} s | GPU Mem Peak {:.4f} GB"
+            .format(epoch, epoch_time[-1], mem_list[-1]))
 
     # use the first epoch to warm up
-    print('Average epoch sampling time:', np.mean(epoch_time[1:])*1000," ms")
-    print('Average epoch gpu mem peak:', np.mean(mem_list[1:])," GB")
+    print('Average epoch sampling time:',
+          np.mean(epoch_time[1:]) * 1000, " ms")
+    print('Average epoch gpu mem peak:', np.mean(mem_list[1:]), " GB")
     print('####################################################END')
     with open("../outputs/result.csv", "a") as f:
         writer = csv.writer(f, lineterminator="\n")
         avg_sampling_time = np.mean(epoch_time[1:])
         if args.dataset == 'friendster':
             # to align with papers result
-            avg_sampling_time /=10
+            avg_sampling_time /= 10
         log_info = ["gSampler", args.dataset, avg_sampling_time, "node2vec"]
         writer.writerow(log_info)
         print(f"result writen to ../outputs/result.csv")
 
-def load(dataset,args):
+
+def load(dataset, args):
     device = args.device
     use_uva = args.use_uva
     g, features, labels, n_classes, splitted_idx = dataset
     sample_list = []
     static_memory = torch.cuda.memory_allocated()
     train_nid = splitted_idx['train']
-    
+
     if args.data_type == 'int':
         g = g.int()
         train_nid = train_nid.int()
@@ -109,9 +121,7 @@ def load(dataset,args):
         g = g.long()
         train_nid = train_nid.long()
     csc_indptr, csc_indices, edge_ids = g.adj_sparse('csc')
-    # train_nid = train_nid.int()
-    # csc_indptr = csc_indptr.int()
-    # csc_indices = csc_indices.int()
+
     if use_uva and device == 'cpu':
         csc_indptr = csc_indptr.pin_memory()
         csc_indices = csc_indices.pin_memory()
@@ -119,39 +129,62 @@ def load(dataset,args):
         csc_indptr = csc_indptr.to('cuda')
         csc_indices = csc_indices.to('cuda')
 
-    m = gs.Matrix(gs.Graph(False))
-    m._graph._CAPI_load_csc(csc_indptr, csc_indices)
-    print("Check load successfully:", m._graph._CAPI_metadata(), '\n')
+    m = gs.matrix_api.Matrix()
+    m.load_graph("CSC", [csc_indptr, csc_indices])
+    bm = gs.matrix_api.BatchMatrix()
+    bm.load_from_matrix(m, False)
     train_nid = train_nid.to('cuda')
-    # csc_indptr, csc_indices, edge_ids = g.adj_sparse('csc')
-    del g
-    benchmark_w_o_relabel(args, m, train_nid)
 
+    del g
+    benchmark_w_o_relabel(args, bm, train_nid)
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--device", default='cuda', choices=['cuda', 'cpu'],
+    parser.add_argument("--device",
+                        default='cuda',
+                        choices=['cuda', 'cpu'],
                         help="Training model on gpu or cpu")
-    parser.add_argument('--use-uva', action=argparse.BooleanOptionalAction,
-                        help="Wether to use UVA to sample graph and load feature")
-    parser.add_argument("--dataset", default='products', choices=['reddit', 'products', 'papers100m','friendster','livejournal'],
+    parser.add_argument(
+        '--use-uva',
+        action=argparse.BooleanOptionalAction,
+        help="Wether to use UVA to sample graph and load feature")
+    parser.add_argument("--dataset",
+                        default='products',
+                        choices=[
+                            'reddit', 'products', 'papers100m', 'friendster',
+                            'livejournal'
+                        ],
                         help="which dataset to load for training")
-    parser.add_argument("--batchsize", type=int, default=128,
+    parser.add_argument("--batchsize",
+                        type=int,
+                        default=128,
                         help="batch size for training")
-    parser.add_argument("--num-workers", type=int, default=0,
-                        help="numbers of workers for sampling, must be 0 when gpu or uva is used")
-    parser.add_argument("--num-epoch", type=int, default=3,
+    parser.add_argument(
+        "--num-workers",
+        type=int,
+        default=0,
+        help=
+        "numbers of workers for sampling, must be 0 when gpu or uva is used")
+    parser.add_argument("--num-epoch",
+                        type=int,
+                        default=3,
                         help="numbers of epoch in training")
-    parser.add_argument("--data-type", default='long', choices=['int', 'long'],
+    parser.add_argument("--data-type",
+                        default='long',
+                        choices=['int', 'long'],
                         help="data type")
-    parser.add_argument("--walk-length", type=int, default=80,
+    parser.add_argument("--walk-length",
+                        type=int,
+                        default=80,
                         help="random walk walk length")
     parser.add_argument("--samples",
                         default='25,10',
                         help="sample size for each layer")
-    parser.add_argument("--big-batch", type=int, default=1280,
+    parser.add_argument("--big-batch",
+                        type=int,
+                        default=12800,
                         help="big batch")
     args = parser.parse_args()
     print('Loading data')
@@ -165,8 +198,7 @@ if __name__ == '__main__':
         dataset = load_livejournal()
     print(dataset[0])
 
-
-# bench('DGL random walk', dgl_sampler, g, 4, iters=10, node_idx=nodes)
-# bench('Matrix random walk Non-fused', matrix_sampler_nonfused, matrix,
-#       4, iters=10, node_idx=nodes)
-    load(dataset,args)
+    # bench('DGL random walk', dgl_sampler, g, 4, iters=10, node_idx=nodes)
+    # bench('Matrix random walk Non-fused', matrix_sampler_nonfused, matrix,
+    #       4, iters=10, node_idx=nodes)
+    load(dataset, args)
