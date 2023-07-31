6bcc156825acca0967f89be8193bb2e0ef0787ef
Ping Gong <33480480+gpzlx1@users.noreply.github.com>
2023-08-01 04:47:09 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,16 +4,13 @@ export epochs
 training_epochs=2
 export training_epochs
 
-
-
 echo "run figure7"
 cd figure7/
 bash testbench.sh
 
 echo "run figure8"
 cd ../figure8
-bash testbench.sh
+source testbench.sh
 
 cd ../figure10
-bash testbench.sh
-
+source testbench.sh
